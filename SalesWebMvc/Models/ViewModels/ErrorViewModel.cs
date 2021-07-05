48a58d1629dae55ec6ed09ee1022100d2ef41053
Guilherme Dutra <guilherme.ale.dutra@gmail.com>
2021-07-05 14:35:53 -0300

    Projeto SalesWebMvc

---
+++ SalesWebMvc/Models/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SalesWebMvc.Models.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string Message { get; set; }
+        public string RequestId { get; set; }
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
